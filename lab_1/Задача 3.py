7826f1d8508eb483b6161bd26847d9741b2f98ce
shpalshpalych <bp043917@gmail.com>
2023-09-22 09:35:57 +0300

    test

--- lab_1/Задача 3.py
+++ lab_1/Задача 3.py
@@ -2,7 +2,7 @@ import random
 
 ran_numbers = []
 for _ in range(20):
-    ran_numbers.append(random.randint(1,100))
+    ran_numbers.append(random.randint(1, 100))
 print(ran_numbers)
 unique_numbers = [x for x in ran_numbers if ran_numbers.count(x) <= 2]
 unique_numbers.sort(reverse=True)
